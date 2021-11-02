898f94e0eda9922fce4999e071c2ae3335f3c6fb
longwang235 <longwang235@126.com>
2021-11-02 15:33:22 +0800

    重构gf.check_play_button，新开游戏的重置项目整合至gf.start_game

--- game_functions.py
+++ game_functions.py
@@ -51,26 +51,31 @@ def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens,
     """在玩家单击Play按钮时开始新游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
-        # 重置游戏设置
-        ai_settings.initialize_dynamic_settings()
+        start_game(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
-        # 隐藏光标
-        pygame.mouse.set_visible(False)
 
-        # 重置游戏统计信息
-        stats.reset_stats()
-        stats.game_active = True
+def start_game(ai_settings, screen, stats, sb, ship, aliens, bullets):
+    """开始新游戏，重置设置"""
+    # 重置游戏设置
+    ai_settings.initialize_dynamic_settings()
 
-        # 重置记分牌图像
-        sb.prep_images()
+    # 隐藏光标
+    pygame.mouse.set_visible(False)
 
-        # 清空外星人列表和子弹列表
-        aliens.empty()
-        bullets.empty()
+    # 重置游戏统计信息
+    stats.reset_stats()
+    stats.game_active = True
 
-        # 创建一群新的外星人，并让飞船居中
-        create_fleet(ai_settings, screen, ship, aliens)
-        ship.center_ship()
+    # 重置记分牌图像
+    sb.prep_images()
+
+    # 清空外星人列表和子弹列表
+    aliens.empty()
+    bullets.empty()
+
+    # 创建一群新的外星人，并让飞船居中
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
 
 
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
