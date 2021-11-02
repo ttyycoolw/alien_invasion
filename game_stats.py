d621b81f957994c25bf8297d97e3229bf808d5ec
longwang235 <longwang235@126.com>
2021-11-02 13:58:57 +0800

    python教程原始项目-外星人入侵

---
+++ game_stats.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Time    : 2021/10/16 29:40
+# @Author  : wangming
+# @FileName: game_stats.py
+
+
+class GameStats:
+    """跟踪有戏的统计信息"""
+
+    def __init__(self, ai_settings):
+        """初始化统计信息"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+
+        # 游戏刚启动时处于关闭活动状态
+        self.game_active = False
+
+        # 在任何情况下都不应该重置最高得分
+        self.high_score = 0
+
+    def reset_stats(self):
+        """初始化在有戏运行期间可能变化的统计信息"""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
